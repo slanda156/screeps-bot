e9c1a79ebc17371a8d673f799abb99fd3d775196
Christoph Heil <christoph.heil156@gmail.com>
2025-12-04 21:08:32 +0100

    Removed most logic for fresh start

--- role.harvester.js
+++ role.harvester.js
@@ -1,31 +1,7 @@
 var roleHarvester = {
-
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(!creep.memory.mining && creep.store[RESOURCE_ENERGY] == 0) {
-            creep.memory.mining = true;
-	    }
-	    if(creep.memory.mining && creep.store.getFreeCapacity() == 0) {
-	        creep.memory.mining = false;
-	    }
-	    if(creep.memory.mining) {
-            var sources = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
-            if(creep.harvest(sources) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources);
-            }
-        }
-        else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_EXTENSION
-                                ) && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
-            });
-            if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targets[0]);
-            }
-        }
+
 	}
 };
 
